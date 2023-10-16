8feac06d483b495cb6b87c24ac51903aa0920b93
Luizyy <analuizynascimentodasilva@gmail.com>
2023-10-16 15:44:48 -0300

    multa.

--- atividade10.py
+++ atividade10.py
@@ -1,7 +1,7 @@
 # Exercício Python 10: faça um algoritimo que receba a velocidade em Km/h de um veiculo e:
 # se maior que 60km/h aplicar multa de 7 vezes a da velocidade permitida
 km = float(input("Insira a velocidade em Km/h percorrida:"))
-multa= km*7
+multa= (km-60)*7
 if km > 60:
     print(f"Voce recebera uma multa de:{multa} reais")
 else:
